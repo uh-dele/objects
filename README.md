9a39bafbdc42ae39fbfd8cfd7c91da6518708173
Alison Abreu-Garcia <alison.iag@gmail.com>
2016-03-01 15:33:41 -0500

    initialize

---
+++ README.md
@@ -0,0 +1,3 @@
+## Objects
+
+[View assignment instructions on the class website.](http://creativecomputing-s16.github.io/assignments/objects.html)
---
+++ sketch.js
@@ -0,0 +1,18 @@
+//========================
+//	City
+//========================
+
+//your sun/moon object:
+
+//your building object:
+
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+
+}
+
+function draw(){
+
+
+}
